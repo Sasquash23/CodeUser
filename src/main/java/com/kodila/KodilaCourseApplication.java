c29db71a5b7fd1c8610629228e5d65feac5ce125
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-06-21 05:38:02 +0200

    First commit

---
+++ src/main/java/com/kodila/KodilaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.kodila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodilaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodilaCourseApplication.class, args);
+    }
+
+}
