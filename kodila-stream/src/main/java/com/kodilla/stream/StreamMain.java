04089f7be6851e35f079afa3cc1de267eab95df6
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-06-26 18:46:06 +0200

    Module 7.2. Stream - usage to iterate

--- kodila-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodila-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.lambda.SaySomething;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
@@ -40,5 +40,8 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
