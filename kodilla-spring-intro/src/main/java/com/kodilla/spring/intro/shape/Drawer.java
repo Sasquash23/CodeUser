ba2d6a604bb185fbe4bab8dc723e5254b0cf473a
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-09-16 04:21:15 +0200

    Module 14 Spring 5 introduction

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -0,0 +1,39 @@
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+    final Shape shape;
+
+    public Drawer(final Shape shape) {
+        this.shape = shape;
+    }
+
+    public String doDrawing() {
+        return shape.draw();
+    }
+    //public void doDrawing() {
+        /*polymorphism 1
+        Circle circle = new Circle();
+        Triangle triangle = new Triangle();
+
+        circle.draw();
+        triangle.draw();*/
+
+        /*polymorphism 2
+        Figure figure;
+
+        figure = new Circle();
+        figure.draw();
+
+        figure = new Triangle();
+        figure.draw();*/
+
+        /*polymorphism 3
+        Shape shape;
+
+        shape = new Circle();
+        shape.draw();
+
+        shape = new Triangle();
+        shape.draw();
+    }*/
+}
