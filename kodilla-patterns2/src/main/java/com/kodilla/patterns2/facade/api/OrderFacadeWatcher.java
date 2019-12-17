b36db86a2d3fad85fce9a61fc6e7f31acd70d3b0
Sasquash23 <agnieszka.sklorz@gmail.com>
2019-12-17 22:15:47 +0100

    Module 24.3. Watcher - added forOrderFacede

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,26 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order,userId) && target(object)")
+    public void logEvent(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Watcher for OrderFacade:\n" +
+                "Class: " + object.getClass().getName() + "\n" +
+                "Args: OrderDto: " + order.getItems().stream()
+                .map(n->n.toString())
+                .collect(Collectors.joining(",\n")) +
+                "UserId: " + userId);
+    }
+}
